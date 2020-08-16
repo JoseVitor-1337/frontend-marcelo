5f4196e570ef9961bdc9b4b0d6f68cd948d79d09
JoseVitor-1337 <josevitordemelo@yahoo.com>
2020-08-16 19:12:54 -0400

    feature: Setup of frontend developement was successfully maded

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Oh Hello There</p>
+    </div>
+  );
+}
+
+export default App;
