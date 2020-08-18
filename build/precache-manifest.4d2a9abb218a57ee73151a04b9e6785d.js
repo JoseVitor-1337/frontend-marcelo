ff31ee97610c99a98076d5e95bcc1654db08caff
JoseVitor-1337 <josevitordemelo@yahoo.com>
2020-08-18 17:30:53 -0400

    feature: change the colors of the application.

---
+++ build/precache-manifest.4d2a9abb218a57ee73151a04b9e6785d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241c3c9fd7706fd41ee69d5c0c4012f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb0999a6d926d89d626",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "d2bde46cc3a25655af30",
+    "url": "/static/css/main.c632f5c4.chunk.css"
+  },
+  {
+    "revision": "6eb0999a6d926d89d626",
+    "url": "/static/js/2.d6d6c1eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6d6c1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bde46cc3a25655af30",
+    "url": "/static/js/main.a8e0a48a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "93f3e5d3f503dd71b450c6f3e9a1c435",
+    "url": "/static/media/check.93f3e5d3.svg"
+  },
+  {
+    "revision": "6898db0386805b40db82405451e79087",
+    "url": "/static/media/password-hidden.6898db03.svg"
+  },
+  {
+    "revision": "1e3617991d7942b0278b8f434211679f",
+    "url": "/static/media/password-visible.1e361799.svg"
+  }
+]);
