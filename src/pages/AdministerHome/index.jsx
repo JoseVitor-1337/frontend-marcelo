766e4f2101e87e502b680065793b68ff4f36acaf
JoseVitor-1337 <josevitordemelo@yahoo.com>
2020-08-21 18:04:16 -0400

    Feature: make the responsive better.

--- src/pages/AdministerHome/index.jsx
+++ src/pages/AdministerHome/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavigationHeader } from "../../components/Navigation";
+import { SearchInput } from "../../components/Inputs";
 import "./style.css";
 
 const AdministerHome = () => {
   return (
     <>
       <NavigationHeader />
+      <SearchInput />
     </>
   );
 };
